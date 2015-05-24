5fb95453539e17589be52ab970df44225fe25a52
Okiriza Wibisono <okiriza@users.noreply.github.com>
2015-05-24 22:05:06 +0700

    Create taksi.R

---
+++ skrip/R/taksi.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+
+set.seed(100)
+
+x <- rnorm(100, 5, 1.8)
+y <- 7.5 + x*15 + rnorm(100)*10
+d <- data.frame(Jarak=x, Tarif=y)
+
+ggplot(d, aes(Jarak, Tarif)) + 
+    geom_point(col='blue', size=3) +
+    geom_smooth(method='lm', size=1, col='red', se=F, fullrange=T) + 
+    xlim(0, 10) + ylim(0, 150) + 
+    xlab('Jarak (km)') + ylab('Tarif (ribu rupiah)') + 
+    theme(axis.title=element_text(size=15), axis.text=element_text(size=12))
